261ba670496197130cccabc0a0579ddbef05796b
nishithak212 <knishitha95@gmail.com>
2025-02-10 23:19:03 -0500

    Initial commit with folder structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.scss'
+import photos from "./data/photos.json";
+import tags from "./data/tags.json";
+
+function App() {
+
+  console.log("Photos:",photos);
+  console.log("Tags",tags);
+  return (
+    <>
+    <h1>Snaps</h1>
+    </>
+  )
+}
+
+export default App
